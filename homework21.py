92f0f3028b7c8b514a01ed089994561f2d0ecf6e
IvanOgurtsov1996 <jjgraffity@gmail.com>
2023-12-20 15:29:21 +0300

    Fixing pipeline

--- homework21.py
+++ homework21.py
@@ -1,43 +1,45 @@
+"""Homework 21: Testing AtomCounter Class"""
+
 import pytest
 from AtomCounter import AtomCounter
 
 
-def test_count_atoms_positive_1():
+def test_count_atoms_positive_1():  # Test counting atoms in a simple formula.
     formula = 'H2O'
     counter = AtomCounter(formula)
     atoms = counter.count_atoms()
     assert atoms == {'H': 2, 'O': 1}
 
 
-def test_count_atoms_positive_2():
+def test_count_atoms_positive_2():  # Test counting atoms in a formula with parentheses and numbers.
     formula = '(NaCl)2'
     counter = AtomCounter(formula)
     atoms = counter.count_atoms()
     assert atoms == {'Na': 2, 'Cl': 2}
 
 
-def test_count_atoms_positive_3():
+def test_count_atoms_positive_3():  # Test counting atoms in a complex formula.
     formula = 'C6H12O6'
     counter = AtomCounter(formula)
     atoms = counter.count_atoms()
     assert atoms == {'C': 6, 'H': 12, 'O': 6}
 
 
-def test_count_atoms_negative_1():
+def test_count_atoms_negative_1():  # Test counting atoms in an invalid formula (unbalanced parentheses).
     formula = '('
     counter = AtomCounter(formula)
     atoms = counter.count_atoms()
     assert atoms == {}
 
 
-def test_count_atoms_negative_2():
+def test_count_atoms_negative_2():  # Test counting atoms in an invalid formula (unbalanced parentheses).
     formula = 'H2O)'
     counter = AtomCounter(formula)
     atoms = counter.count_atoms()
     assert atoms == {'H': 2, 'O': 2}
 
 
-def test_count_atoms_negative_3():
+def test_count_atoms_negative_3():  # Test counting atoms in an invalid formula (unclosed bracket).
     formula = 'H2[O'
     counter = AtomCounter(formula)
     atoms = counter.count_atoms()
